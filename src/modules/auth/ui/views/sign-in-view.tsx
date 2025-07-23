2839b578af6dfc8985c9a63ce67b24891ffe61d0
Kurt Ivan C. Quejada <kurtquejada@gmail.com>
2025-07-24 00:03:15 +0800

    authentication UI

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,173 @@
+"use client";
+import { z } from "zod";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { OctagonAlert } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1, { message: "Password is Required" }),
+});
+
+export function SignInView() {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0 ">
+        <CardContent className="grid p-0 md:grid-cols-2 ">
+          <SignIpForm />
+          <div className="bg-radial from-primary to-chart-5 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="logo.svg" alt="LOGO" className="h-[92px] w-[92px] " />
+            <p className="text-2xl font-semibold text-primary-foreground">
+              Talk With Ai
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-sm text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By Clicking continue, you agree to our <a href="#">Terms Of Services</a>
+        and <a href="#">Privacy Policy</a>
+      </div>
+    </div>
+  );
+}
+
+function SignIpForm() {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleOnSubmit = (data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+    authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          setError(error.message);
+        },
+      }
+    );
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleOnSubmit)}
+        className="p-6 md:p-8 "
+      >
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center text-center">
+            <h1 className="text-2xl font-bold">Welcome Back</h1>
+            <p className="text-muted-foreground text-balance">
+              Login to your Account
+            </p>
+          </div>
+          <div className="grid gap-3">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="you@example.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>{" "}
+          <div className="grid gap-3">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {!!error && (
+            <Alert className="bg-destructive/10 border-none">
+              <OctagonAlert className="h-4 w-4 !text-destructive" />
+              <AlertTitle>{error}</AlertTitle>
+            </Alert>
+          )}
+          <Button disabled={pending} type="submit" className="w-full">
+            Sign In
+          </Button>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="bg-card text-muted-foreground relative z-10 px-2">
+              Or Continue With
+            </span>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Button
+              disabled={pending}
+              variant="secondary"
+              type="button"
+              className="w-full"
+            >
+              GOOGLE
+            </Button>
+            <Button
+              disabled={pending}
+              variant="secondary"
+              type="button"
+              className="w-full"
+            >
+              GITHUB
+            </Button>
+          </div>
+          <div className="text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/sign-up" className="underline underline-offset-4">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+}
