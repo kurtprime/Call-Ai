d791d58a91a3368535188a1aca98979a74ffa20f
Kurt Ivan C. Quejada <kurtquejada@gmail.com>
2025-07-30 20:14:29 +0800

    Agent set up for comminting

--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export function HomeView() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "BITGGER" }));
-
-  return <div>{data?.greeting}</div>;
+  return <div>HOME VIEW</div>;
 }
