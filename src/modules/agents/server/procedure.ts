1636ab1664bc9f6b341f492018b45f54d60209c6
Kurt Ivan C. Quejada <kurtquejada@gmail.com>
2025-08-02 20:13:17 +0800

    support

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -22,7 +22,12 @@ export const agentsRouter = createTRPCRouter({
     }),
 
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        ...getTableColumns(agents),
+        meetingCount: sql<number>`5`,
+      })
+      .from(agents);
 
     return data;
   }),
